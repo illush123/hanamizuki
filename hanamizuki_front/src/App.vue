6418629fbd715ab154727320fcb8b10c19557daa
Kazuya_Nagase <al15065@shibaura-it.ac.jp>
2018-12-29 23:11:13 +0900

    add bootstrap

--- hanamizuki_front/src/App.vue
+++ hanamizuki_front/src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <li><a href="https://twitter.com/vuejs" target="_blank" class="btn btn-dark">Twitter</a></li>
     </div>
     <router-view/>
   </div>
