8b7387415412904f6e5c2618829cf3abd4cd0e57
sitRyo <al15030@shibaura-it.ac.jp>
2019-01-01 23:23:17 +0900

    bootstrapが動かない

---
+++ hanamizuki_front/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="header-wrapper">
+      <div class="header-text">レッスン一覧</div>
+      <div class="header-text-lesson_message">{{message}}</div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
