a05d0693d4dad4099c7ae13da297b049c5cca66b
Kazuya_Nagase <al15065@shibaura-it.ac.jp>
2019-01-01 23:50:49 +0900

    bootstrap4導入

--- hanamizuki_front/src/views/Home.vue
+++ hanamizuki_front/src/views/Home.vue
@@ -2,12 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button type="button" class="btn btn-dark">Dark</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 @Component({
   components: {
