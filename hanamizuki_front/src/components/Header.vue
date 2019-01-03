0f54b9f5b75d5b8e594e48a17658ba78d9a108dd
sitRyo <al15030@shibaura-it.ac.jp>
2019-01-03 12:07:31 +0900

    router変更, Header要素変更

--- hanamizuki_front/src/components/Header.vue
+++ hanamizuki_front/src/components/Header.vue
@@ -1,14 +1,22 @@
 <template>
   <header>
-    <div class="header-wrapper">
-      <div class="header-text">レッスン一覧</div>
-      <div class="header-text-lesson_message">{{message}}</div>
+    <div class="header-image-wrapper">
+      <img src="/path/to/image" class="header-image_src">
+    </div>
+    <div class="header-test-wrapper">
+      <span class="header-text-lesson">レッスン一覧</span>
+      <span class="header-text-lesson_name">{{lessonTitle}}</span>
     </div>
   </header>
 </template>
 
 <script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
+@Component
+export default class HelloWorld extends Vue {
+  // @Prop() private msg!: string;
+}
 </script>
 
 <style scoped>
--- hanamizuki_front/src/views/Home.vue
+++ hanamizuki_front/src/views/Home.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Header></Header>
     <button type="button" class="btn btn-dark">Dark</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Header from '@/components/Header.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 @Component({
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Header,
   },
 })
 export default class Home extends Vue {}
