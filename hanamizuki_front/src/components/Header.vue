352fae985f4588aeba4fba388c0e4b7dd2dd36f7
sitRyo <al15030@shibaura-it.ac.jp>
2019-01-04 12:21:12 +0900

    marginとpaddingをbootstrap仕様に変更

--- hanamizuki_front/src/components/Header.vue
+++ hanamizuki_front/src/components/Header.vue
@@ -1,26 +1,29 @@
 <template>
   <header>
-    <div class="header-image-wrapper">
-      <img src="/path/to/image" class="header-image_src">
-    </div>
-    <div class="header-text-wrapper">
-      <div class="header-text-lesson_wrapper">
-        <span class="header-text-lesson">レッスン一覧</span>
+    <div class="container">
+      <div class="header-image-wrapper pr-4">
+        <img src="/path/to/image" class="header-image_src">
       </div>
-      <span class="header-text-lesson_name">{{lessonTitle}}</span>
+      <div class="header-text-wrapper">
+        <div class="header-text-lesson-wrapper pr-2">
+          <div class="header-text-lesson ml-5">レッスン一覧</div>
+        </div>
+        <div class="header-text-lesson_name pt-1 pl-3">{{lessonTitle}}</div>
+      </div>
     </div>
   </header>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 @Component
 export default class Header extends Vue {
-  @Prop({ default: 'dummy message' }) private messageProp!: string;
 
   // data
-  lessonTitle = this.messageProp;
+  lessonTitle = 'これはダミータイトルです';
 }
 </script>
 
@@ -35,31 +38,24 @@ export default class Header extends Vue {
 
     .header-text-wrapper {
       width: 80%;
-      // height: auto;
+      height: 100%;
       text-align: left;
-      border-bottom: 3px solid #a9eda9;
-      padding-bottom: 10px;
 
-      .header-text-lesson_wrapper {
+      .header-text-lesson-wrapper {
         font-size: 15px;
         font-weight: 600;
-        padding-bottom: 4px;
-        padding-right: 10px;
 
         .header-text-lesson {
           background-color: #B8E994;
           border: solid 5px #B8E994;
           border-radius: 0.5em;
+          float: left;
         }
       }
 
-      .header-text-lesson {
-        float: left;
-        margin-left: 32px;
-      }
 
       .header-text-lesson_name {
-        padding-left: 11px;
+        float: left;
       }
     }
   }
