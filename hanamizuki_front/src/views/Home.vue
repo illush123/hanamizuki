a8c0343ba2d1bb19bba2b25ba5676167ad3c10c6
sitRyo <al15030@shibaura-it.ac.jp>
2019-01-04 12:20:42 +0900

    値の受け渡し部分削除, コメント削除

--- hanamizuki_front/src/views/Home.vue
+++ hanamizuki_front/src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
-    <Header messageProp="this is propmessage"></Header>
+    <Header></Header>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import Header from '@/components/Header.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 @Component({
   components: {
-    // HelloWorld,
     Header,
   },
 })
